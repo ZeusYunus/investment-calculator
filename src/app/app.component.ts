2a5c9659c66a53cfa9be8c9bf7a98b707d05c5cc
Nizaam Yunus <nizaamcoleyunus18@gmail.com>
2025-07-20 13:29:44 +0200

    Outputting to a table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,24 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserInputComponent } from "./user-input/user-input.component";
 import type { InvestmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
 export class AppComponent {
+  resultData?: {
+    year: number,
+    interest: number,
+    valueEndOfYear: number,
+    annualInvestment: number,
+    totalInterest: number,
+    totalAmountInvested: number,
+  }[];
+
   onCalculateInvestmentResults(data: InvestmentInput) {
     const { initialInvestment, annualInvestment, expectedReturn, duration } = data;
     
@@ -32,7 +42,6 @@ export class AppComponent {
       });
     }
 
-    // return annualData;
-    console.log(annualData);
+    this.resultData = annualData;
   }
 }
